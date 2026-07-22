bea4918e3351abd0040a6a9fe82fa7b73647f4d4
Argelecciones <argelecciones@gmail.com>
2026-07-21 21:17:49 -0300

    Delete src directory

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-  import { useRoute, useRouter } from 'vue-router';
-
-  const route = useRoute();
-  const router = useRouter();
-</script>
-<template>
-  <RouterView></RouterView>
-</template>
--- src/diagram/index.js
+++
@@ -1,4 +0,0 @@
-export { Party } from "./party";
-export { Diagram } from "./diagram";
-export { ArchDiagram } from "./archDiagram";
-export { WestminsterDiagram } from "./wmDiagram";
